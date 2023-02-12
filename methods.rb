bebb3cf81ffd1412f0d36ffa9e04e427f7f06abc
Casey Ochieng <caseyochieng46@gmail.com>
2023-02-12 21:31:17 +0300

    new attempt

--- methods.rb
+++ methods.rb
@@ -1,46 +1,28 @@
-#  Your code here! 
-  
-def greet_programmer() 
-  puts "Hello, progrmmer!" 
- end 
-  
- greet_programmer 
-  
- def greet(name ="Naureen") 
-   puts "Hello, #{name}!" 
- end 
-  
- greet 
-  
-def greet(name ="Jimmy") 
-     puts "Hello, #{name}!" 
-   end 
-  
- greet 
-  
- 
- 
- 
-def greet_with_default(name ="Naureen") 
-   puts "Hello, #{name}!" 
-  
-end 
-  
-  
-def greet_with_default(name ="programmer") 
-     
- puts "Hello, #{name}!" 
-    
- end 
-    
-greet_with_default 
-  
-  
-def add (num1 ,num2) 
-     num1+num2 
-   
-end 
-  
-def halve() 
- return nil unless nummer.to_a?(Integer) 
+# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+
+end
+
+def greet name
+    puts "Hello, #{name}!"
+
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+
+end
+
+def add num1, num2
+    num1+num2
+
+end
+
+def halve number
+    if number.class != Integer
+        return nil     
+    end
+  number/2
+
 end
